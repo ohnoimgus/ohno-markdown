987d5aff8285c68fbd439edcb94f95f2209bbea4
Gustav Eiman <gustav.eiman@svt.se>
2023-02-17 22:22:42 +0100

    whopsie on the content type but now actually fixed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
 import aiofiles
 from markdown import markdown
 from aiocache import cached, Cache
@@ -14,10 +15,9 @@ async def document(file_name='index'):
 
 async def serve_document(file_name):
     try:
-        return await get_file(file_name)
+        return HTMLResponse(await get_file(file_name))
     except FileNotFoundError:
-        raise HTTPException(status_code=404, detail='Whops!', headers={
-                            'Content-Type': 'text/html'})
+        raise HTTPException(status_code=404, detail='Whops!')
 
 
 @cached(cache=Cache.MEMORY)
